e77555b7d4f7c7e93942ba6f3e34fe4ff49b6d13
Joe Quattrone <joequattrone5@gmail.com>
2018-11-28 18:29:57 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,25 @@
-
+class Book
+  attr_accessor :author, :genre
+  
+  def initialize(title)
+    @title = title
+  end
+  
+  def title
+    @title
+  end
+  
+  def page_count=(page_count)
+    @page_count = page_count
+  end
+  
+  def page_count
+    @page_count
+  end
+  
+  def turn_page
+    puts"Flipping the page...wow, you read fast!"
+  end
+  
+end
 
